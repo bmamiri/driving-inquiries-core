24eb0f0dac2f80ed0fd9600aad92e32e88f84919
Mohammad Amiri <30575897+bmamiri@users.noreply.github.com>
2019-03-21 21:32:03 +0330

    Initial Project

---
+++ src/main/java/com/vu/DrivingInquiriesCoreApplication.java
@@ -0,0 +1,13 @@
+package com.vu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrivingInquiriesCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DrivingInquiriesCoreApplication.class, args);
+    }
+
+}
