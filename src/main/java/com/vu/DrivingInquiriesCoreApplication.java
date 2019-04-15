3a313f236a31ae8e558d02d69e78fc4660208835
Mohammad Amiri <b.m.amiri@live.com>
2019-04-15 10:34:13 +0430

    Config for deploy project WAR file

--- src/main/java/com/vu/DrivingInquiriesCoreApplication.java
+++ src/main/java/com/vu/DrivingInquiriesCoreApplication.java
@@ -2,9 +2,10 @@ package com.vu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class DrivingInquiriesCoreApplication {
+public class DrivingInquiriesCoreApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(DrivingInquiriesCoreApplication.class, args);
